f388c5a2e5df517bf99d9f6c4257ce14fc4d1ac8
Gregory-hub <novikov.gri@gmail.com>
2026-07-20 02:28:38 +0300

    Implement team system

--- Source/RogueLike/Characters/CharacterBase.h
+++ Source/RogueLike/Characters/CharacterBase.h
@@ -2,9 +2,11 @@
 
 #pragma once
 
-#include "AbilitySystemInterface.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameplayTagContainer.h"
+
+#include "AbilitySystemInterface.h"
 
 #include "CharacterBase.generated.h"
 
@@ -25,24 +27,38 @@ public:
     UFUNCTION( BlueprintCallable, BlueprintPure, Category = "CharacterBase" )
     UBillboardComponent* GetBillboardComponent() const
     {
-        return BillboardComponent_;
+        return BillboardComponent;
     }
 
     UFUNCTION( BlueprintCallable, BlueprintPure, Category = "CharacterBase" )
     virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override
     {
-        return AbilitySystemComponent_;
+        return AbilitySystemComponent;
+    }
+    
+    UFUNCTION( BlueprintCallable, BlueprintPure, Category = "CharacterBase" )
+    const TSet<FGameplayTag>& GetAllyTags() const
+    {
+        return AllyTags;
     }
 
 protected:
+    virtual void BeginPlay() override;
+
     virtual void PossessedBy( AController* NewController ) override;
 
     UPROPERTY( VisibleAnywhere )
-    TObjectPtr<UBillboardComponent> BillboardComponent_;
+    TObjectPtr<UBillboardComponent> BillboardComponent;
 
     UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = "CharacterBase" )
-    TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent_;
+    TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
 
     UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = "CharacterBase" )
-    TObjectPtr<UBasicAttributeSet> BasicAttributeSet_;
+    TObjectPtr<UBasicAttributeSet> BasicAttributeSet;
+
+    UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "CharacterBase" )
+    TSet<FGameplayTag> ActorTags;
+    
+    UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "CharacterBase", meta = ( ToolTip = "Characters with this tag are not hit by this character") )
+    TSet<FGameplayTag> AllyTags;
 };
