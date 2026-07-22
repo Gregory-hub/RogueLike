c912e850f795b50fff953d805951bf467832140e
Gregory_hub <52703175+Gregory-hub@users.noreply.github.com>
2026-07-22 22:29:43 +0300

    Gregory/create refactor abilities (#7)
    * Create explision ability
    
    * Implement team system
    
    * Add mouse input to throw explosion and add self explosion ability

--- Source/RogueLike/Characters/CharacterBase.cpp
+++ Source/RogueLike/Characters/CharacterBase.cpp
@@ -15,13 +15,13 @@ ACharacterBase::ACharacterBase()
     GetCharacterMovement()->bOrientRotationToMovement = true;
     GetCharacterMovement()->RotationRate = FRotator( 0.f, TNumericLimits<double>::Max(), 0.f );
 
-    BillboardComponent_ = CreateDefaultSubobject<UBillboardComponent>( TEXT( "BillboardComponent" ) );
-    BillboardComponent_->SetupAttachment( GetRootComponent() );
-    BillboardComponent_->SetHiddenInGame( false );
+    BillboardComponent = CreateDefaultSubobject<UBillboardComponent>( TEXT( "BillboardComponent" ) );
+    BillboardComponent->SetupAttachment( GetRootComponent() );
+    BillboardComponent->SetHiddenInGame( false );
 
-    AbilitySystemComponent_ = CreateDefaultSubobject<UAbilitySystemComponent>( TEXT( "AbilitySystemComponent" ) );
+    AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>( TEXT( "AbilitySystemComponent" ) );
 
-    BasicAttributeSet_ = CreateDefaultSubobject<UBasicAttributeSet>( TEXT( "Attributes" ) );
+    BasicAttributeSet = CreateDefaultSubobject<UBasicAttributeSet>( TEXT( "Attributes" ) );
 }
 
 void ACharacterBase::Move( const FVector2D& Vector )
@@ -33,9 +33,19 @@ void ACharacterBase::Move( const FVector2D& Vector )
     AddMovementInput( FVector::RightVector, Vector.X );
 }
 
+void ACharacterBase::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    for ( auto& Tag : ActorTags )
+    {
+        Tags.Add( Tag.GetTagName() );
+    }
+}
+
 void ACharacterBase::PossessedBy( AController* NewController )
 {
     Super::PossessedBy( NewController );
 
-    AbilitySystemComponent_->InitAbilityActorInfo( this, this );
+    AbilitySystemComponent->InitAbilityActorInfo( this, this );
 }
